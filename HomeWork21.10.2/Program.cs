04a9a3743fc1b597a8834ca4975ba58af3f58b84
Void <Void@DESKTOP-LJJ5G9O>
2023-10-21 12:52:13 +0300

    Add project files.

---
+++ HomeWork21.10.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ArithemicExpressionCalculator
+{
+    static void Main()
+    {
+        Console.Write("Enter an arithmetic expression with + and - operators: ");
+        string expression = Console.ReadLine();
+
+        int result = CalculateArithmeticExpression(expession);
+        Console.WriteLine("Result: " + result);
+    }
+}
